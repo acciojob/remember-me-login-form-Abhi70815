191d553598d7b9d9a32e200c712bae17b592bcc7
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-07-10 23:03:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ let checkbox=document.getElementById('checkbox');
 let username=document.getElementById('username');
 let password=document.getElementById('password');
 document.getElementById('submit').addEventListener('click',(e)=>{
-    e.preventDefault()
-	if(checkbox.checked==true){
-localStorage.setItem("username",username.value);
-localStorage.setItem("password",password.value);
+    e.preventDefault();
+    if(checkbox.checked==true){
+        localStorage.setItem("username",username.value);
+        localStorage.setItem("password",password.value);
+        alert("Logged in as " + username.value);
     }
     else{
- localStorage.removeItem('username');
- localStorage.removeItem('password');
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
     }
-})
+});
 
 if (localStorage.getItem('username') && localStorage.getItem('password')) {
     document.getElementById('existing').style.display = 'block';
@@ -21,8 +22,5 @@ if (localStorage.getItem('username') && localStorage.getItem('password')) {
 }
 
 document.getElementById('existing').addEventListener('click',()=>{
-if(username.value==localStorage.getItem('username') && password.value==localStorage.getItem('password')){
-	alert("Logged in as "+localStorage.getItem('username'));
-
-}
-})
+    alert("Logged in as " + localStorage.getItem('username'));
+});
