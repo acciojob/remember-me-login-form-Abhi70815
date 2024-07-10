3cb1c4acdff4f4cc2ab3eeac2c2471e3ba4ec3f2
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-07-10 22:15:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+document.getElementById('submit').addEventListener('click',(e)=>{
+    e.preventDefault()
+let checkbox=document.getElementById('checkbox');
+let username=document.getElementById('username');
+let password=document.getElementById('password');
+	if(checkbox.checked==true){
+localStorage.setItem("username",username.value);
+localStorage.setItem("password",password.value);
+    }
+    else{
+ localStorage.removeItem('username');
+ localStorage.removeItem('password');
+    }
+})
